7eba73c64001b8f7dbd482c767bf959cec17ac3a
Kirill <kirill.kanabay@gmail.com>
2021-05-05 13:46:56 +0300

    Добавлен контроллер работы со счетами

---
+++ WorldYachtsApi/Controllers/InvoicesController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WorldYachts.Data.Entities;
+using WorldYachts.Services.Contracts;
+using WorldYachts.Services.Invoices;
+using WorldYachtsApi.Helpers;
+
+namespace WorldYachtsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController:ControllerBase
+    {
+        private readonly IInvoiceService _invoiceService;
+        private readonly IMapper _mapper;
+        private readonly ILogger<InvoicesController> _logger;
+
+        public InvoicesController(IInvoiceService invoiceService, IMapper mapper, ILogger<InvoicesController> logger)
+        {
+            _invoiceService = invoiceService;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        // GET: api/<InvoicesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Getting all invoices");
+            return Ok(_invoiceService.GetAll());
+        }
+
+        // GET api/<InvoicesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            _logger.LogInformation($"Getting invoice by id:{id}");
+            var response = await _invoiceService.GetByIdAsync(id);
+            if (!response.IsSuccess)
+            {
+                _logger.LogError(response.Message);
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+        // POST api/<InvoicesController>
+        [HttpPost]
+        [Authorize("Admin", "Sales Person")]
+        public async Task<IActionResult> Post([FromBody] Invoice invoice)
+        {
+            var response = await _invoiceService.AddAsync(invoice);
+            _logger.LogInformation($"Adding invoice (Id:{invoice.Id})");
+            if (!response.IsSuccess)
+            {
+                _logger.LogError(response.Message);
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+        // PUT api/<InvoicesController>/5
+        [HttpPut("{id}")]
+        [Authorize("Admin", "Sales Person")]
+        public async Task<IActionResult> Put(int id, [FromBody] Invoice invoice)
+        {
+            var response = await _invoiceService.UpdateAsync(id, invoice);
+
+            _logger.LogInformation($"Updating invoice (id:{id})");
+
+            if (!response.IsSuccess)
+            {
+                _logger.LogError(response.Message);
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+        // DELETE api/<InvoicesController>/5
+        [HttpDelete("{id}")]
+        [Authorize("Admin", "Sales Person")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _invoiceService.DeleteAsync(id);
+            _logger.LogInformation($"Deleting invoice (id:{id})");
+            if (!response.IsSuccess)
+            {
+                _logger.LogError(response.Message);
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+    }
+}
---
+++ WorldYachtsApi/Serialization/InvoiceMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WorldYachts.Data.Entities;
+
+namespace WorldYachtsApi.Serialization
+{
+    public class InvoiceMapper:Profile
+    {
+        public InvoiceMapper()
+        {
+            //Invoice -> Invoice
+            CreateMap<Invoice, Invoice>()
+                .ForMember(dst => dst.Date, opt => opt.MapFrom(src => src.Date))
+                .ForMember(dst => dst.ContractId, opt => opt.MapFrom(src => src.ContractId))
+                .ForMember(dst => dst.Settled, opt => opt.MapFrom(src => src.Settled))
+                .ForMember(dst => dst.Sum, opt => opt.MapFrom(src => src.Sum))
+                .ForMember(dst => dst.SumInclVat, opt => opt.MapFrom(src => src.SumInclVat))
+                .ForMember(dst => dst.Id, opt => opt.Ignore())
+                .ForMember(dst => dst.Contract, opt => opt.Ignore())
+                ;
+        }
+    }
+}
